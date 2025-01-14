59c8646aaeb3a33ce6727a8fbb485591309b7488
Lee Sanghyun <143057125+SangHyun01@users.noreply.github.com>
2025-01-14 10:30:48 +0900

    [level 0] Title: A로 B 만들기, Time: 0.79 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
@@ -0,0 +1,5 @@
+function solution(before, after) {
+    let beforeStr = before.split("").sort().join("");
+    let afterStr = after.split("").sort().join("");
+    return beforeStr === afterStr ? 1 : 0;
+}
