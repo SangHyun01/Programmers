3b978e327148c07ebdf9988c835168435f91b24d
Lee Sanghyun <143057125+SangHyun01@users.noreply.github.com>
2025-08-05 18:20:55 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.23 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
@@ -1,6 +1,16 @@
 function solution(n) {
     let arr = String(n).split("");
-    console.log(arr);
-    let result = arr.sort((a,b) => b-a);
-    return Number(result.join(""));
+    for(let i=0; i<arr.length - 1; i++){
+        for(let j=0; j<arr.length - 1-i ; j++){
+            if(arr[j] < arr[j+1]){
+                [arr[j],arr[j+1]] = [arr[j+1],arr[j]];
+            }
+        }
+    }
+    return Number(arr.join(""))
 }
+
+
+ // let arr = String(n).split("");
+    // let result = arr.sort((a,b) => b-a);
+    // return Number(result.join(""));
