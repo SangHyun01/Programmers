686c278513b43cb0dd56a14e5357b2bdd949d475
Lee Sanghyun <143057125+SangHyun01@users.noreply.github.com>
2025-09-17 13:23:42 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 37.94 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -0,0 +1,44 @@
+function solution(maps) {
+    // 최단 거리 구하는 문제 -> BFS
+    const n = maps.length; // 맵 세로 길이
+    const m = maps[0].length; // 맵 가로 길이
+    
+    // 맵과 동일한 크기로 visited 배열 만들고 초기화
+    const visited = Array.from({length: n}, () => new Array(m).fill(false));
+    
+    // 큐에 [x,y,거리] 저장
+    const queue = [];
+    
+    // 상,하,좌,우 방향 나타나는 배열
+    const dx = [0,0,1,-1];
+    const dy = [1,-1,0,0];
+    
+    // 탐색 시작점
+    queue.push([0,0,1]);
+    visited[0][0] = true;
+    
+    // BFS 루프 시작
+    while(queue.length > 0) {
+        const [x,y,dist] = queue.shift();
+        
+        // 목적지에 도착했는지 확인
+        if(x === n-1 && y === m-1) {
+            return dist;
+        }
+        
+        for(let i=0; i<4; i++) {
+            const nextX = x + dx[i];
+            const nextY = y + dy[i];
+            
+            // 다음 위치가 유효한지 확인
+            if(nextX >= 0 && nextX < n && nextY >= 0 && nextY < m) {
+                if(maps[nextX][nextY] === 1 && !visited[nextX][nextY]) {
+                    visited[nextX][nextY] = true;
+                    queue.push([nextX, nextY, dist + 1]);
+                }
+            }
+        }
+    }
+    
+    return -1;
+}
