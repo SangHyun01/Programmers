bb1073a081a51fa570c92b0d7b4542abf4160507
Lee Sanghyun <143057125+SangHyun01@users.noreply.github.com>
2025-10-21 19:53:33 +0900

    [level 1] Title: 폰켓몬, Time: 0.64 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,11 +1,16 @@
 function solution(nums) {
-    const set = new Set(nums);
-    return set.size >= Math.floor(nums.length/2) ? Math.floor(nums.length/2) : set.size;
+    // 중복 제거
+    let set  = new Set(nums);
+    
+    // 찾고자 하는 개수
+    let find_count = nums.length / 2;
+    
+    // 종류의 개수
+    let counts = set.size;
+    
+    let result = Math.min(find_count, counts);
+    
+    return result
 }
-// nums 를 set으로 저장해서 중복을 없애고
 
-// [3,3,3,2,2,4] -> [3,2,4] 3개
-// [3,1,2,3] -> [3,1,2] 2
-// [3,3,3,2,2,2] -> [3,2] 3
-// set한거의 size >= nums/2 return=nums/2
-// size < nums/2 return=size
+
