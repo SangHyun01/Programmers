c8b83509252984d64728101d9206b86f1124c67d
Lee Sanghyun <143057125+SangHyun01@users.noreply.github.com>
2025-10-28 18:03:15 +0900

    [level 3] Title: 네트워크, Time: 0.79 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,33 +1,30 @@
 function solution(n, computers) {
-    var answer = 0;
-    const visited = new Array(n).fill(false);
+    // 컴퓨터 개수만큼 방문 배열 만들고 false 초기화
+    let visited = new Array(n).fill(false);
     
-    for(let i=0; i<n; i++) {
-        // 이 컴퓨터를 방문하지 않았으면
-        if(!visited[i]) {
-            // 새로운 네트워크 발견했으므로
-            answer++;
-            bfs(i, n, computers, visited)
-        }
-    }
-    return answer;
-}
-
-function bfs(start, n, computers, visited) {
-    const queue = [start];
-    visited[start] = true;
+    // 네트워크 셀 개수
+    let answer = 0;
     
-    while(queue.length > 0) {
-        const currentNode = queue.shift();
+    // dfs 함수
+    function dfs(currentNode) {
+        // 현재 노드 방문 처리
+        visited[currentNode] = true;
         
-        for(let j=0; j<n; j++) {
-            // 자기 자신이 아니고
-            // 다른 컴퓨터와 연결되어있고
-            // 방문하지 않은 것
-            if(currentNode !== j && computers[currentNode][j] === 1 && !visited[j]) {
-                visited[j] = true;
-                queue.push(j);
+        for(let neighbor=0; neighbor < n; neighbor++) {
+            if(computers[currentNode][neighbor] === 1 && !visited[neighbor]) {
+                dfs(neighbor);
             }
         }
     }
+    for(let i=0; i<n; i++) {
+        // 아직 i번째 컴퓨터 방문 안했음으면
+        if(!visited[i]) {
+            answer++;
+            
+            dfs(i);
+        }
+    }
+    
+    return answer;
 }
+
