bd52264aec76b6e135c27144a76ea4c3c186263d
Lee Sanghyun <143057125+SangHyun01@users.noreply.github.com>
2025-11-04 15:14:07 +0900

    [level 2] Title: 피보나치 수, Time: 2.80 ms, Memory: 37.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    let fib = Array(n+1);
+    
+    fib[0] = 0;
+    fib[1] = 1;
+    
+    for(let i=2; i<= n; i++) {
+        fib[i] = (fib[i-1] + fib[i-2]) % 1234567;
+    }
+    
+    return fib[n];
+}
