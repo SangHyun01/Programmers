b120c3b6907cc190063b3da10eca73ba572b07ad
Lee Sanghyun <143057125+SangHyun01@users.noreply.github.com>
2025-11-23 17:09:39 +0900

    [level 2] Title: 귤 고르기, Time: 34.88 ms, Memory: 51.7 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,16 +1,31 @@
 function solution(k, tangerine) {
-    const cnt = new Map();
+    const kind = new Map();
     
-    for(const x of tangerine) cnt.set(x,(cnt.get(x) ?? 0) + 1);
-    
-    const sizes = Array.from(cnt.values()).sort((a,b) => (b-a));
-    
-    let usedKinds = 0;
-    
-    for(const c of sizes){
-        k -= c;
-        usedKinds += 1;
-        if(k <= 0) return usedKinds;
+    for(const t of tangerine) {
+        kind.set(t, (kind.get(t) || 0) + 1);
     }
-    return usedKinds;
+    const counts = [...kind.values()];
+    
+    counts.sort((a,b) => b-a);
+    
+    let answer = 0;
+    let sum = 0;
+    
+    for(const count of counts) {
+        answer++;
+        sum += count;
+        
+        if(sum >= k) break;
+    }
+    return answer;
 }
+
+
+// 1 2 2 3 3 4 5 5
+
+// 2233455
+// 많이 있는 것부터 담음
+
+// 1 2 2 3 3 4 5 5  4개
+
+// 그럼 각 크기:개수를 맵으로 저장
